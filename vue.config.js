8a5c5f087c360217b215b6954b74a0dcbca1270e
Wuu-Server <1249732457@qq.com>
2021-08-21 16:52:11 +0800

    🔧 配置本地服务器跨域，通过/local进行本地服务器请求

--- vue.config.js
+++ vue.config.js
@@ -6,4 +6,17 @@ module.exports = {
       },
     },
   },
+  // 配置本地跨域
+  devServer: {
+    proxy: {
+      "/local": {
+        target: "http://localhost:3000/api",
+        ws: true,
+        changeOrigin: true,
+        pathRewrite: {
+          "^/local": "", // rewrite path
+        },
+      },
+    },
+  },
 };
